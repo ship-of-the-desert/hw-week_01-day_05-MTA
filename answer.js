d46bea95e237af46e46eccd3bf94c588320a5c34
MuhammadHakami <mmm.mephiles@gmail.com>
2019-03-03 09:08:31 +0300

    fixed a bug

--- answer.js
+++ answer.js
@@ -24,10 +24,12 @@ var trainStops=function(i1,i2,i3,i4) {
     // the complete process with one liner
     (i1===i3)? console.log("You must travel through the following stops on the N line: "+step(i1,i2,i4)+'\n'+step(i1,i2,i4).length+" stops in total."):(map[i1].indexOf(i2)<map[i1].indexOf('Union Square'))? step1=step(i1,i2,'Union Square'):step1=step(i1,i2,'Union Square',reverse=true);(map[i3].indexOf(i4)>map[i3].indexOf('Union Square'))? step2=step(i3,'Union Square',i4):step2=step(i3,'Union Square',i4,reverse=true);
     // console.log the steps
+    if (i1!==i3){
     console.log("You must travel through the following stops on the N line: "+step1+'.');
     console.log("Change at Union Square.");
     console.log("Your journey continues through the following stops: "+step2+'.');
     console.log((step1.length+step2.length-1+" stops in total."));
+    }
 }
 // running the function
-trainStops('N','Times Square','6','33rd');
+trainStops('N','Times Square','N','23rd');
