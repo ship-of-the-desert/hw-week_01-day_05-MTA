b11e62a357b8dd8f77ffe5f46551d2839c351c05
lamyahamid <lamya.altuwairqi@gmail.com>
2019-03-03 09:04:18 +0300

    my homework

---
+++ hw5.js
@@ -0,0 +1,13 @@
+//
+
+
+function planTrip(srart_line, start_stop, end_line, end_stop){
+    var lineOne = ["Times Square", "34th", "28th", "23rd", "Union Square",""];
+    var lineTow = ["8th", "6th", "Union Square", "3rd","1st"];
+    var lineTree = ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"]; 
+    if (srart_line==lineOne)
+    for (var i in lineOne){
+        
+        
+    }
+}
